9a4e8b4ae0e9627210223719cbda59dd6903fb65
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2021-01-12 23:24:55 -0500

    Update king-of-the-clicks.py

--- king-of-the-clicks.py
+++ king-of-the-clicks.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
+from random import randrange
 import time
 
 url = "https://kingoftheclicks.com/?ref=thekingoftheclicks"
-browser = webdriver.Chrome('/Users/isaiah/Desktop/Code/Python/chromedriver')
+browser = webdriver.Chrome('/Users/isaiah/Desktop/Code/Python/king-of-the-clicks/chromedriver')
 browser.get(url)
 
 time.sleep(2)
 browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[2]/div/div/div/footer/button[1]').click()
 time.sleep(2)
 
-while True:
+i = 1
+
+while(True):
     browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span').click()
+    print(i)
+    i = i+1
     browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span').click()
+    print(i)
+    i = i+1
     browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span').click()
+    print(i)
+    i = i+1
