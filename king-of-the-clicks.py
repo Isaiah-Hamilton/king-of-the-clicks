10ae9d3686d130525653262ee35fc68b5c9cb0a2
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2021-01-13 14:12:35 -0500

    Update king-of-the-clicks.py

--- king-of-the-clicks.py
+++ king-of-the-clicks.py
@@ -10,10 +10,5 @@ time.sleep(2)
 browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[2]/div/div/div/footer/button[1]').click()
 time.sleep(2)
 
-i = 1
-
 while(True):
     browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span').click()
-    print(i)
-    i = i+1
-
