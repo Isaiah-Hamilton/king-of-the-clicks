a69612de4fb723fa7a9f2ca6e49e2b07a1220b54
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2021-01-13 13:56:49 -0500

    Update king-of-the-clicks.py

--- king-of-the-clicks.py
+++ king-of-the-clicks.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from random import randrange
 import time
+#pip install selenium for the project to work.
 
 url = "https://kingoftheclicks.com/?ref=thekingoftheclicks"
 browser = webdriver.Chrome('/Users/isaiah/Desktop/Code/Python/king-of-the-clicks/chromedriver')
@@ -16,9 +16,4 @@ while(True):
     browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span').click()
     print(i)
     i = i+1
-    browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span').click()
-    print(i)
-    i = i+1
-    browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span').click()
-    print(i)
-    i = i+1
+
