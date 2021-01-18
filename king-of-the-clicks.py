dfc5b7ffa9f1251c81a8e54127489743f0a5cc51
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2021-01-17 23:23:19 -0500

    Update king-of-the-clicks.py

--- king-of-the-clicks.py
+++ king-of-the-clicks.py
@@ -8,7 +8,7 @@ browser.get(url)
 
 time.sleep(2)
 browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[2]/div/div/div/footer/button[1]').click()
-time.sleep(2)
+time.sleep(1)
 
 while(True):
     browser.find_element_by_xpath('//*[@id="__layout"]/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span').click()
