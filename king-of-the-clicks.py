59420924d292c003e192bea77f525c93aa580f35
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2021-03-18 17:07:26 -0400

    Update king-of-the-clicks.py

--- king-of-the-clicks.py
+++ king-of-the-clicks.py
@@ -3,7 +3,7 @@ import time
 #pip install selenium and time for the project to work.
 
 url = "https://kingoftheclicks.com/?ref=thekingoftheclicks"
-browser = webdriver.Chrome('LOCATION OF FOR THE CHROME DRIVER')
+browser = webdriver.Chrome('LOCATION OF THE CHROME DRIVER')
 browser.get(url)
 
 time.sleep(2)
